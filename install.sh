5e93b57329d839b792106a187ae28ad909cd11c5
Luke <luke_13@sjtu.edu.cn>
2019-12-05 02:32:38 +0800

    Update cp back up suffix in install.sh.

--- install.sh
+++ install.sh
@@ -11,22 +11,22 @@ rm install.sh README.md archive.sh
 rm -rf .git
 
 # Do not override git settings
-if [ -e ~/.gitconfig.bak ]; then
-    mv ~/.gitconfig.bak ~/.gitconfig
+if [ -e ~/.gitconfig.\~1\~ ]; then
+    mv ~/.gitconfig.\~1\~ ~/.gitconfig
 fi
 
 # ZSH environment setup
 if [[ -d ~/.oh-my-zsh ]]; then
-    if [[ ! -d ~/.oh-my-zsh.bak ]]; then
-        mkdir ~/.oh-my-zsh.bak
+    if [[ ! -d ~/.oh-my-zsh.\~1\~ ]]; then
+        mkdir ~/.oh-my-zsh.\~1\~
     fi
-    cp -rf  ~/.oh-my-zsh/* ~/.oh-my-zsh.bak
+    cp -rf  ~/.oh-my-zsh/* ~/.oh-my-zsh.\~1\~
 fi
 rm -rf .oh-my-zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-if [[ -d ~/.oh-my-zsh.bak ]]; then
-    cp -rf ~/.oh-my-zsh.bak/custom ~/.oh-my-zsh/
+if [[ -d ~/.oh-my-zsh.\~1\~ ]]; then
+    cp -rf ~/.oh-my-zsh.\~1\~/custom ~/.oh-my-zsh/
 fi
 
 # VIM environment setup
