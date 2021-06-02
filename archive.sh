466a0e8f1993e1c5bb8b5a302928908d35d6e6aa
Luke <luke_13@sjtu.edu.cn>
2021-06-02 11:40:03 +0800

    Add entries in archive.sh.

--- archive.sh
+++ archive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~
 tar czvf ~/dotfiles.tgz .oh-my-zsh .pip .vim .condarc .zshrc .gitconfig .npmrc \
-    .tmux.conf .tmux.conf.local .vimrc
+    .tmux.conf .tmux.conf.local .vimrc .config/coc .config/nvim
